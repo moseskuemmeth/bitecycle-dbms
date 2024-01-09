208c0808af00c1341d99723b5bd107e6d684e74c
Vatsal Pandey <vatsalpandey2110@gmail.com>
2024-01-09 22:42:05 +0530

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Faq from "./pages/Faq";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/faq" activeStyle>
+            FAQ
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
